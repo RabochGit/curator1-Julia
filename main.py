e51e4f80626925aa931911e0c4ce0afd85343212
Gruvi1 <asjj@gmail.com>
2023-11-29 15:39:41 +0700

    khj

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ import telebot
 bot = telebot.TeleBot('6562163720:AAFG17bguAPuAPjPuHz-whunRar-ArWcHck')
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['Hello!'])
 def main(message):
     bot.send_message(message.chat.id, 'Привет! Меня зовут ТелеНав. Я отправлю тебе 3 сообщения')
-    bot.send_message(message.chat.id, 'Сегодня у тебя будет *замечательный* день')
+
+
+@bot.message_handler(commands=['first_message'])
+def main(message):
+    bot.send_message(message.chat.id, 'Сегодня у тебя будет *замечательный* день', pars_mode='Markdown')
+
+
+@bot.message_handler(commands=['second_message'])
+def main(message):
     bot.send_message(message.chat.id, 'С вами приятно иметь дело!')
-    bot.send_message(message.chat.id, 'Удачного дня!')
+
+
+@bot.message_handler(commands=['third_message'])
+def main(message):
+    bot.send_message(message.chat.id, 'Удачного дня!', pars_mode='Markdown')
 
 
 bot.infinity_polling()
